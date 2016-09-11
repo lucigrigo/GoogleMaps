cacadff21e19d61c4a7214a468f3a2fd70c68f26
lucigrigo <lucigrigo@yahoo.com>
2016-09-11 15:31:10 +0300

    P1

---
+++ GoogleMaps/DBManagement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GoogleMaps
+{
+    class DBManagement
+    {
+        private string cones = Properties.Settings.Default.DBConnection;
+        private SqlCommand scom=null;
+        private SqlDataReader sreader=null;
+        private DataTable tab=null;
+        private SqlConnection sc=null;
+
+        public DataTable Load(string tableName)
+        {
+            sc = new SqlConnection(cones);
+            sc.Open();
+            scom = new SqlCommand("SELECT * FROM " + tableName, sc);
+            sreader = scom.ExecuteReader();
+            tab = new DataTable();
+            tab.Load(sreader);
+
+            return tab;
+        }
+    }
+}
---
+++ GoogleMaps/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace GoogleMaps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void LoadData()
+        {
+            DataTable dt = new DBManagement().Load("Countries");
+
+            foreach(DataRow dr in dt.Rows)
+            {
+                treeView1.Nodes.Add(new TreeNode(dr["Name"].ToString()));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
